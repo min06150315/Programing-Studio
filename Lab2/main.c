202abdcc079d7ad60dbae76f570d473a0d9c5f3d
min06150315 <min06150315@gmail.com>
2024-03-14 22:36:20 +0900

    Lab2 HW final

--- Lab2/main.c
+++ Lab2/main.c
@@ -133,11 +133,25 @@ void findClasses(char* name, struct st_class* c[], int csize){
 int addNewClass(struct st_class* c[], int csize){
 // Caution : Don't allow the duplicate class code.
 // You must complete this function.
-
 	struct st_class* p = (struct st_class*)malloc(sizeof(struct st_class));
+	
+	//	class code duplication check
+	while (1){
+		printf(">> code number > ");
+		scanf("%d", &(p->code));
 
-	printf(">> code number > ");
-	scanf("%d", &(p->code));
+		int check = 0;
+		for (int i = 0; i < csize; i++){
+			if (p->code == c[i]->code){
+				check = 1;
+				printf(">> Code duplicated! Retry.\n");
+				break;
+			}
+		}
+		if (!check)
+			break;
+	}
+
 	printf(">> class name > ");
 	scanf("%s", p->name);
 	printf(">> credits > ");
@@ -152,47 +166,112 @@ int addNewClass(struct st_class* c[], int csize){
 void editClass(struct st_class* c[], int csize){
 	struct st_class* p;
 	int code;
-	printf(">> Enter a code of class > ");
-	scanf("%d", &code);
 
-	
-	// You must complete this section.
+	while (1){
+		printf(">> Enter a code of class > ");
+        scanf("%d", &code);
 
+		//	check a code of class
+		int check = 0;
+		for (int i = 0; i < csize; i++){
+			if (code == c[i]->code){
+					check = 1;
+					p = c[i];
+					printf("> Current: [%d] %s [credits %d - %s]\n",p->code, p->name, p->unit, kname[p->grading-1]);
+					printf("> Enter new class name > ");
+					scanf("%s", p->name);
+					printf("> Enter new credits > ");
+					scanf("%d", &(p->unit));
+					printf("> Enter new grading(1:Grade, 2: P/F) > ");
+					scanf("%d", &(p->grading));
 
-	
-	printf("> Current: [%d] %s [credits %d - %s]\n",p->code, p->name, p->unit, kname[p->grading-1]);
-	printf("> Enter new class name > ");
-	scanf("%s", p->name);
-	printf("> Enter new credits > ");
-	scanf("%d", &(p->unit));
-	printf("> Enter new grading(1:Grade, 2: P/F) > ");
-	scanf("%d", &(p->grading));
-
-	printf("> Modified.\n");
-
+					printf("> Modified.\n");
+					c[i] = p;
+					break;
+			}
+		}
+		if (check)
+			break;
+		else
+			printf("> Please enter code again\n");
+	}
 }
 
 
 // You must make all these functions.
 
 int applyMyClasses(int my[], int msize, struct st_class* c[], int csize){
+	int code;
+	while(1){
+		printf(">> Enter a class code > ");
+		scanf("%d", &code);
+		int check = 0;
+		for (int i = 0; i < csize; i++){
+			if (code == c[i]->code){
+				my[msize] = code;
+				msize++;
+				printf(">> [%d] %s [credits %d - %s]\n", c[i]->code, c[i]->name, c[i]->unit, kname[c[i]->grading-1]);
+				check = 1;
+				break;
+			}
+		}
 
+		if (!check){
+			printf(">> No such code of class.\n");
+			continue;
+		}
+		printf(">> Add more?(1:Yes 2:No) > ");
+		scanf("%d", &check);
 
-
-	
-	return 0;
+		if(check == 1)
+			continue;
+		else if (check == 2)
+			break;
+	}
+	return msize;
 }
 
 void printMyClasses(int my[], int msize, struct st_class* c[], int csize){
-
-
-	
-
+	int credit = 0;
+	for (int i = 0; i < msize; i++){
+		for (int j = 0; j < csize; j++){
+			if (my[i] == c[j]->code){
+				printf("%d. [%d] %s [credits %d - %s]\n",i + 1 ,c[j]->code, c[j]->name, c[j]->unit, kname[c[j]->grading - 1]);
+				credit += c[j]->unit;
+			}
+		}
+	}
+	printf("All : %d credits\n", credit);
 }
 
 void saveMyClass(int my[], int msize, struct st_class* c[], int csize){
-
-
-
-	
+	int credit = 0;
+	int creditA = 0, creditP = 0;
+	//	file open
+	FILE *fp = NULL;
+	fp = fopen("my_classes.txt", "w");
+	//	error check
+	if (fp == NULL)
+    {
+        printf("Failed to open the file my_classes.txt\n");
+        exit(-1);
+    }
+	//	file write
+	fprintf(fp, "My Classes\n");
+	for (int i = 0; i < msize; i++){
+		for (int j = 0; j < csize; j++){
+			if (my[i] == c[j]->code){
+				fprintf(fp, "%d. [%d] %s [credits %d - %s]\n", i + 1, c[j]->code, c[j]->name, c[j]->unit, kname[c[j]->grading - 1]);
+				//credit count
+				credit += c[j]->unit;
+				if (strcmp(kname[c[j]->grading - 1], "A+~F") == 0)
+					creditA += c[j]->unit;
+				else if (strcmp(kname[c[j]->grading - 1], "P/F") == 0)
+					creditP += c[j]->unit;
+				break;
+			}
+		}
+	}
+	fprintf(fp ,"All : %d classes, %d credits (A+~F %d credits, P/F %d credits)\n", msize, credit, creditA, creditP);
+	fclose(fp);
 }
