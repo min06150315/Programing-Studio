513a1cd5fac2a093976e23a39b9548b2c8b33ae3
min06150315 <min06150315@gmail.com>
2024-03-22 02:16:52 +0900

    made function makeReport

--- Lab3/C_lab3.c
+++ Lab3/C_lab3.c
@@ -310,6 +310,62 @@ int deleteChannel(struct st_channel* c[], int size){
 
 
 void makeReport(struct st_channel* c[], int size){
+	FILE *fp = NULL;
+	fp = fopen("report.txt", "w");
 
+	fprintf(fp, "Channel List\n");
+	for (int i = 0; i < size; i++){
+		fprintf(fp, "[%2d] %-20s %10d peoples [%s] \n", i + 1, c[i]->name, c[i]->count, LNAME[c[i]->level]);
+	}
 
+	// Reuse the printStatistics() function
+	fprintf(fp, "\nStatistics of Channels\n");
+	int count[5] = {0};
+    int sub_sum[5] = {0};
+    float sub_avg[5] = {0};
+    char top[5][30];
+    int top_people[5] = {0};
+    for (int i = 0; i < size; i++){
+        if (c[i]->count < 1000){
+            count[0]++;
+            sub_sum[0] += c[i]->count;
+            if(c[i]->count > top_people[0]){
+                top_people[0] = c[i]->count;
+                strcpy(top[0], c[i]->name);
+            }
+        }else if (c[i]->count < 10000){
+            count[1]++;
+            sub_sum[1] += c[i]->count;
+            if(c[i]->count > top_people[1]){
+                top_people[1] = c[i]->count;
+                strcpy(top[1], c[i]->name);
+            }
+        }else if (c[i]->count < 100000){
+            count[2]++;
+            sub_sum[2] += c[i]->count;
+            if(c[i]->count > top_people[2]){
+                top_people[2] = c[i]->count;
+                strcpy(top[2], c[i]->name);
+            }
+        }else if (c[i]->count < 1000000){
+            count[3]++;
+            sub_sum[3] += c[i]->count;
+            if(c[i]->count > top_people[3]){
+                top_people[3] = c[i]->count;
+                strcpy(top[3], c[i]->name);
+            }
+        }else {
+            count[4]++;
+            sub_sum[4] += c[i]->count;
+            if(c[i]->count > top_people[4]){
+                top_people[4] = c[i]->count;
+                strcpy(top[4], c[i]->name);
+            }
+        }
+    }
+	for (int i = 0; i < 5; i++){
+			sub_avg[i] = (float)sub_sum[i] / count[i];
+			fprintf(fp, "%s : %d channels, Average %.1f, Top channel : %s (%d peoples)\n", LNAME[i], count[i], sub_avg[i], top[i], top_people[i]);
+		}
+	fclose(fp);
 }
