39bdb41359db5123d77d509bad857c58913a69c6
min06150315 <min06150315@gmail.com>
2024-03-27 22:35:48 +0900

    Topic  Select

--- Lab4/C_lab4.c
+++ Lab4/C_lab4.c
@@ -1,5 +1,12 @@
+// 자유 주제 CRUD프로그램 제작
+// Create / Read / Update / Delete / Search / Save
+// 주제 : CRUD를 활용한 간단한 RPG 게임 만들기
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int main()
 {
