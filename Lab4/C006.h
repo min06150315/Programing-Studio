abfdeb27dedd0d9d29eca99d45089beec714920b
min06150315 <min06150315@gmail.com>
2024-03-29 11:04:02 +0900

    Update Header File

--- Lab4/C006.h
+++ Lab4/C006.h
@@ -8,8 +8,6 @@ struct Player{
     int grade; // 1 ~ 5 grades
     int noJob; // 1 ~ 10 jobs
 };
-char GNAME[5][20] = {"Bronze", "Silver", "Gold", "Platinum", "Diamond"};
-char JNAME[5][20] = {"Warrior", "Wizard", "Archer", "Assasian", "Pirate"};
 
 void printPlayer(struct Player* p[], int psize);
 int addPlayer(struct Player* p[], int psize);
